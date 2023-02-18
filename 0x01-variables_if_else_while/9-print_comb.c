0b51d6569c7204b5bbdc77f91e685c4b7a85e0fd
brianmaiko <maikobrian2@gmail.com>
2023-02-18 01:49:08 -0800

    all possible combinations of single-digit nums

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,38 +7,17 @@
 
 int main(void)
 {
-	int i, j, k, l;
+	int d;
 
-	for (i = 0; i < 10; i++)
+	for (d = '0'; d <= '9'; d++)
 	{
-		putchar('0' + i);
-		putchar(' ');
-
-			for (j = i + 1; j < 10; j++)
-			{
-			putchar('0' + i);
-			putchar('0' + j);
+		putchar(d);
+		if (d != '9')
+		{
+			putchar(',');
 			putchar(' ');
-
-			for (k = j + 1; k < 10; k++)
-			{
-			putchar('0' + i);
-			putchar('0' + j);
-			putchar('0' + k);
-			putchar(' ');
-
-			for (l = k + 1; l < 10; l++)
-			{
-			putchar('0' + i);
-			putchar('0' + j);
-			putchar('0' + k);
-			putchar('0' + l);
-			putchar(' ');
-		}
-		}
 		}
 	}
 	putchar('\n');
 	return (0);
 }
-
