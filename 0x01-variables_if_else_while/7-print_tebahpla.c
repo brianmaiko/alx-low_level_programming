43f58fa52582d3a2f74a9edc24d6ddd65e38ca68
brianmaiko <maikobrian2@gmail.com>
2023-02-17 16:40:01 -0800

    lowercase alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,14 +5,16 @@
  * Return: 0 (success)
  */
 
-int main (void)
+int main(void)
 {
 	char letter = 'z';
+
 	while (letter >= 'a')
 	{
 		putchar(letter);
-		letter --;
+		letter--;
 	}
 	putchar('\n');
+
 	return (0);
 }
