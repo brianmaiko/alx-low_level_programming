3748d28509490131c54720d4ba21c04f2f90b4be
brianmaiko <maikobrian2@gmail.com>
2023-02-21 02:18:49 -0800

    prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,9 +2,7 @@
 
 /**
  * print_alphabet_x10 - entry point
- *
- * Return: Always 0.
- */
+ **/
 
 void print_alphabet_x10(void)
 {
@@ -16,6 +14,5 @@ void print_alphabet_x10(void)
 		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
 		_putchar('\n');
-
-		return (0);
+	}
 }
