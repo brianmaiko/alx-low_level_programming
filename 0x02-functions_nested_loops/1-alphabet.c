254b2a25645e90b2873028214873a504bf95882c
brianmaiko <maikobrian2@gmail.com>
2023-02-21 01:18:16 -0800

    alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - print_alphabet entry point
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+
+{
+	for (c = 'a'; c <= 'z'; c++)
+	{_putchar(c);
+	}
+	_putchar('\n');
+}
