645eba3797def9cf73e7e5f547e15749d80f07f1
brianmaiko <maikobrian2@gmail.com>
2023-02-23 06:31:27 -0800

    update 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int b;
+
+	for (b = 48; b < 58; b++)
+	{
+		_putchar(b);
+	}
+	_putchar('\n');
+}
