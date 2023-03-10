e242e5af4defed9bc6b456d2d15f3c5e8dd4c508
brianmaiko <maikobrian2@gmail.com>
2023-03-10 07:29:02 -0800

    First commit of mylib

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ *
+ * @str: the string to print
+ */
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
