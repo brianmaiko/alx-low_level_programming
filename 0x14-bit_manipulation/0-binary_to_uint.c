841de89416e0f8edbdf7cee1418f03cd2a3bdbca
brianmaiko <maikobrian2@gmail.com>
2023-04-09 02:13:25 -0700

    updated_task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,14 +10,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
+	int n = 0;
 
 	if (!b)
 		return (0);
-	for (; *b; b++)
+
+	while (b[n] != '\0')
 	{
-		if (*b != '0' && *b != '1')
+		if (b[n] != '0' && b[n] != '1')
 			return (0);
-		num = (num <<= 1) | (*b - '0');
+		num = num * 2 + (b[n] - '0');
+		n++;
 	}
 	return (num);
 }
